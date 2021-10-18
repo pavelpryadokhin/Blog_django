bd25947d1c2ceb6ebfc0563cad2e3ff8dc5c2792
Павел <mrpavel23ooo@mail.ru>
2021-10-18 16:25:32 +0300

    #2

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Post
+from .models import Post, Comments
 
 class PostForm(forms.ModelForm):
     class Meta:
         model=Post
         fields=['title','content','status','post_author','image']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model=Comments
+        fields=['comment_text']
